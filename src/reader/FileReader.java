300674a3991f525edb9dfb6582cf44368d0a5d6f
Bryan Chavez <chavezbryanr@gmail.com>
2018-02-03 16:08:12 -0600

    import Java classes into FileReader.java

--- src/reader/FileReader.java
+++ src/reader/FileReader.java
@@ -1,5 +1,13 @@
 package reader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileReader {
+    
+    
+    
 
 }
