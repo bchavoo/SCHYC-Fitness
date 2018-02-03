20c605f08ad6907f0f2a6774e698d19306d252ea
chavezbryanr <chavezbryanr@gmail.com>
2018-02-03 17:02:07 -0600

    Constructors and Get/Sets

--- src/entitites/Person.java
+++ src/entitites/Person.java
@@ -1,26 +1,48 @@
 package entitites;
 
+import java.util.ArrayList;
+
 public class Person {
-	String firstName;
-	String lastName;
+	private String firstName;
+	private String lastName;
+	private ArrayList<String>email;
 	
-	public Person(String firstName, String lastName) {
+	//Constructor
+	public Person(String firstName, String lastName, ArrayList<String> email) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.email = email;
 	}
-	
+
+	//Getters and Setters
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	public ArrayList<String> getEmail() {
+		return email;
+	}
+
+	public void setEmail(ArrayList<String> email) {
+		this.email = email;
+	}
+
+	
+	
 }
+	
+	
+	
