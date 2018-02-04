cf5f4ff9d2bc9970164f3bd285ca4835a56b731c
chavezbryanr <chavezbryanr@gmail.com>
2018-02-04 15:51:07 -0600

    Addjusting for lists? Maybe

--- src/driver/DataConverter.java
+++ src/driver/DataConverter.java
@@ -9,12 +9,14 @@ public class DataConverter {
 	public static void main (String[] args) {
 		
 		//Create a FlatFileReader object
-		FileReader reader = new FileReader();		
+		FileReader reader = new FileReader();	
+		//Returns Person, Member, and Product List
 		
 		//Create JSON Writer Object
-		JSONWriter PersonJSON = new JSONWriter();
-		JSONWriter MemberJSON = new JSONWriter();
-		JSONWriter ProductJSON = new JSONWriter();
+		//Do something like this to use each list returned and convert to output file?
+		JSONWriter PersonJSON = new JSONWriter(personList);
+		JSONWriter MemberJSON = new JSONWriter(memberList);
+		JSONWriter ProductJSON = new JSONWriter(productList);
 		
 		//Create XML Writer Object
 		XMLWriter PersonXML = new XMLWriter();
