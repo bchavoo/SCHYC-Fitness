afa4595f00f02ea0a58943648ae7e1fd6b02749d
chavezbryanr <chavezbryanr@gmail.com>
2018-02-03 20:14:50 -0600

    FileReader - Person

--- src/reader/FileReader.java
+++ src/reader/FileReader.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Comparator;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 import entitites.Member;
 import entitites.Person;
 import entitites.Address;
@@ -17,40 +17,46 @@ public class FileReader {
 	public static void main(String args[]) {
 		
 		String personFile = "data/Persons.dat";
-		Scanner p = null;
+		Scanner s = null;
 		
 		try {
-			p = new Scanner(new File(personFile));
+			s = new Scanner(new File(personFile));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		String person = "";
+		
+		Person person[] = new Person [21];
+
 		
 		
-		int i = 0;
-		while(p.hasNextLine()) {
-			String line = p.nextLine();
-						
-			System.out.println(line);
+		int n = Integer.parseInt(s.nextLine());
+		
+		System.out.println("The value of n is: " + n);
+		
+		while(s.hasNext()) {
+			String line = s.nextLine();
+			String tokenCode[] = line.split(";");
+			String personCode = tokenCode[0];
+			String tokenName[] = tokenCode[1].split(",");
+			String lastName = tokenName[0];
+			String firstName = tokenName[1];
+			String tokenAddress[] = tokenCode[2].split(",");
+			String street = tokenAddress[0];
+			String city = tokenAddress[1];
+			String state = tokenAddress[2];
+			String zip = tokenAddress[3];
+			String country = tokenAddress[4];
 			
- 		}
-		
-		System.out.println(person);
-		
-		
-		
-		
-		
-		
-		//Person person[] = new Person[20];
-		
-		/*
-		for (Person i : person) {
-			person[i] = person;
-			System.out.println(i);
+			
+			System.out.println("Person Code: " + personCode);
+			System.out.println("Name: " + lastName + ", " + firstName);
+			System.out.println("Address: " + street + ", " + city + ", " + zip + ", " + country);
+
 		}
-		*/
+		
+		
+
 		
 		
 	}
