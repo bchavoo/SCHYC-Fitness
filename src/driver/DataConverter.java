43542c215fb093ad2eb974b6822bfd7ff2337b53
chavezbryanr <chavezbryanr@gmail.com>
2018-02-04 15:34:17 -0600

    Added DataConverter JSON and XML Writers

--- src/driver/DataConverter.java
+++ src/driver/DataConverter.java
@@ -13,9 +13,13 @@ public class DataConverter {
 		
 		//Create JSON Writer Object
 		JSONWriter PersonJSON = new JSONWriter();
+		JSONWriter MemberJSON = new JSONWriter();
+		JSONWriter ProductJSON = new JSONWriter();
 		
 		//Create XML Writer Object
 		XMLWriter PersonXML = new XMLWriter();
+		XMLWriter MemberXML = new XMLWriter();
+		XMLWriter ProductXML = new XMLWriter();
 		
 
 	
