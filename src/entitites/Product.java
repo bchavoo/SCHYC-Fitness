c6aeeb34e8213ad6fb19b9ec551357a31c4602be
chavezbryanr <chavezbryanr@gmail.com>
2018-02-03 21:43:55 -0600

    Added Products Inheritance

---
+++ src/entitites/Product.java
@@ -0,0 +1,250 @@
+package entitites;
+
+public class Product {
+
+	private String productCode;
+
+	//Constructor
+	public Product(String productCode) {
+		super();
+		this.productCode = productCode;
+	}
+
+	
+	//Getters and Setters
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	
+	
+	
+	
+	
+	
+	//Year Long Memberships
+	public class YearMemberships extends Product {
+
+		private String productCode;
+		private String productType;
+		private String startDate;
+		private String endDate;
+		private Address address;
+		private String membershipName;
+		private String pricePerUnit;
+
+		//Constructor
+		public YearMemberships(String productCode, String productCode2, String productType, String startDate,
+				String endDate, Address address, String membershipName, String pricePerUnit) {
+			super(productCode);
+			productCode = productCode2;
+			this.productType = productType;
+			this.startDate = startDate;
+			this.endDate = endDate;
+			this.address = address;
+			this.membershipName = membershipName;
+			this.pricePerUnit = pricePerUnit;
+		}
+		// setter and getter 
+		public String getProductCode() {
+			return productCode;
+		}
+
+		public void setProductCode(String productCode) {
+			this.productCode = productCode;
+		}
+
+		public String getProductType() {
+			return productType;
+		}
+
+		public void setProductType(String productType) {
+			this.productType = productType;
+		}
+
+		public String getStartDate() {
+			return startDate;
+		}
+
+		public void setStartDate(String startDate) {
+			this.startDate = startDate;
+		}
+
+		public String getEndDate() {
+			return endDate;
+		}
+
+		public void setEndDate(String endDate) {
+			this.endDate = endDate;
+		}
+
+		public Address getAddress() {
+			return address;
+		}
+
+		public void setAddress(Address address) {
+			this.address = address;
+		}
+
+		public String getMembershipName() {
+			return membershipName;
+		}
+
+		public void setMembershipName(String membershipName) {
+			this.membershipName = membershipName;
+		}
+
+		public String getPricePerUnit() {
+			return pricePerUnit;
+		}
+
+		public void setPricePerUnit(String pricePerUnit) {
+			this.pricePerUnit = pricePerUnit;
+		}
+		
+	}
+	
+	
+	
+	//Day Memberships
+	public class DayMemberships extends Product {
+		
+
+		private String productCode;
+		private String productType;
+		private String dateTime;
+		private Address address;
+		private String cost;
+		
+		//Constructor
+		public DayMemberships(String productCode, String productCode2, String productType, String dateTime, Address address, String cost) {
+			super(productCode);
+			productCode = productCode2;
+			this.productType = productType;
+			this.dateTime = dateTime;
+			this.address = address;
+			this.cost = cost;
+			}
+		
+		//Getter and Setter
+		public String getProductCode() {
+			return productCode;
+		}
+		public void setProductCode(String productCode) {
+			this.productCode = productCode;
+		}
+		public String getProductType() {
+			return productType;
+		}
+		public void setProductType(String productType) {
+			this.productType = productType;
+		}
+		public String getDateTime() {
+			return dateTime;
+		}
+		public void setDateTime(String dateTime) {
+			this.dateTime = dateTime;
+		}
+		public Address getAddress() {
+			return address;
+		}
+		public void setAddress(Address address) {
+			this.address = address;
+		}
+		public String getCost() {
+			return cost;
+		}
+		public void setCost(String cost) {
+			this.cost = cost;
+		}
+	
+		
+	}
+	
+	
+	
+	//ParkingPass
+	public class ParkingPass extends Product {
+		
+		private String productCode;
+		private String productType;
+		private double parkingFee;
+		
+		//Constructor
+		public ParkingPass(String productCode, String productCode2, String productType, double parkingFee) {
+			super(productCode);
+			productCode = productCode2;
+			this.productType = productType;
+			this.parkingFee = parkingFee;
+		}
+
+		//Getters and Setters
+		public String getProductCode() {
+			return productCode;
+		}
+
+		public void setProductCode(String productCode) {
+			this.productCode = productCode;
+		}
+
+		public String getProductType() {
+			return productType;
+		}
+
+		public void setProductType(String productType) {
+			this.productType = productType;
+		}
+
+		public double getParkingFee() {
+			return parkingFee;
+		}
+
+		public void setParkingFee(double parkingFee) {
+			this.parkingFee = parkingFee;
+		}
+		
+		
+	}
+	
+	
+	//Equipment
+	public class Equipment extends Product {
+		private String prodructCode;
+		private String productType;
+		private double cost;
+
+		//Constructor
+		public Equipment(String productCode, String prodructCode, String productType, double cost) {
+			super(productCode);
+			this.prodructCode = prodructCode;
+			this.productType = productType;
+			this.cost = cost;
+		}
+		
+		//Getter and Setter
+		public String getProdructCode() {
+			return prodructCode;
+		}
+		public void setProdructCode(String prodructCode) {
+			this.prodructCode = prodructCode;
+		}
+		public String getProductType() {
+			return productType;
+		}
+		public void setProductType(String productType) {
+			this.productType = productType;
+		}
+		public double getCost() {
+			return cost;
+		}
+		public void setCost(double cost) {
+			this.cost = cost;
+		}
+	}
+
+	
+	
+}
--- src/reader/FileReader.java
+++ src/reader/FileReader.java
@@ -107,7 +107,52 @@ public class FileReader {
 			//Add person to directory
 			//lib.addPerson(p);
 
-				}
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//Products File
+		String productFile = "data/Products.dat";
+		Scanner s = null;
+		
+		try {
+			s = new Scanner(new File(personFile));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+				
+		int k = Integer.parseInt(s.nextLine());
+				
+		System.out.println("The value of n is: " + n);
+				
+		while(s.hasNext()) {
+			String line = s.nextLine();
+			String tokenCode[] = line.split(";");
+			String personCode = tokenCode[0];
+			String tokenName[] = tokenCode[1].split(",");
+			String lastName = tokenName[0];
+			String firstName = tokenName[1];
+			String tokenAddress[] = tokenCode[2].split(",");
+			String street = tokenAddress[0];
+			String city = tokenAddress[1];
+			String state = tokenAddress[2];
+			String zip = tokenAddress[3];
+			String country = tokenAddress[4];
+					
+					
+			//Address a = new Address (street, city, state, zip, country);
+			//Person p = new Person(personCode, firstName, lastName, a, email??);
+					
+			//Add person to directory
+			//lib.addPerson(p);
+
+			}
 		
 	}
     
