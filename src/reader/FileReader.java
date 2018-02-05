2af7a2652b9a402bc23b888c564152eff51e7c43
chavezbryanr <chavezbryanr@gmail.com>
2018-02-05 10:51:38 -0600

    Finishing FileReader

--- src/reader/FileReader.java
+++ src/reader/FileReader.java
@@ -16,17 +16,15 @@ import entitites.Person;
 import entitites.Address;
 import entitites.Product;
 
-	public class FileReader {
-		//Main method below
-		public static void main (String[] args) {
+	public class FileReader {			
 			
-			//private final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("MM-dd-yyyy");
-			//private List<Person> personList = new ArrayList<Person>();
-			//private List<Member> memberList = new ArrayList<Member>();
-		    //private List<Product> productList = new ArrayList<Product>();
+			private final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("MM-dd-yyyy");
+			private List<Person> personList = new ArrayList<Person>();
+			private List<Member> memberList = new ArrayList<Member>();
+		    private List<Product> productList = new ArrayList<Product>();
 			
 			//Person File-------------------------------------------------------------------------------------------------
-		//public List<Person> createPersonList() {
+		public List<Person> createPersonList() {
 			
 			String personFile = "data/Persons.dat";
 			Scanner s = null;
@@ -45,6 +43,8 @@ import entitites.Product;
 			
 			System.out.println("The value of n is: " + n);
 			
+			ArrayList<Person> personList = new ArrayList<Person>();
+			
 			while(s.hasNext()) {
 				String line = s.nextLine();
 				String token[] = line.split(";");
@@ -59,35 +59,40 @@ import entitites.Product;
 				String zip = tokenAddress[3];
 				String country = tokenAddress[4];
 				
+				ArrayList<String> emailArray = new ArrayList<String>();
+				
 				if(token.length == 4) {
 					String tokenEmail[] = token[3].split(",");
+				
 					if(tokenEmail.length == 1) {
 						String email = token[3];
-						System.out.println("Email 1: " + email);
+						emailArray.add(email);
 					} else if (tokenEmail.length == 2) {
 						String email1 = tokenEmail[0];
 						String email2 = tokenEmail[1];
-						System.out.println("Email 1: " + email1);
-						System.out.println("Email 2: " + email2);
+						emailArray.add(email1);
+						emailArray.add(email2);
 					}
-				} else if (token.length == 3){
-					System.out.println("No email");
+					
+				} else if (token.length == 3) {
+					emailArray.add(null);
 				}
 					
 							
+			
 				
 				Address a = new Address (street, city, state, zip, country);
-				Email email = new Email ();
-				Person p = new Person(personCode, firstName, lastName, a, email);
+								
+				Person p = new Person(personCode, firstName, lastName, a, emailArray);
 				
-				//Add person
-				//personList.add(p);
+				//Add person to person List?
+				personList.add(p);
 			}
+			//Return personList?
+			return personList;
+		}
 			
-			//return personList;
-		//}
-			
-			
+		
 	
 			
 			
@@ -95,8 +100,10 @@ import entitites.Product;
 			
 			
 			
-			//Member File-------------------------------------------------------------------------------------------------
-		//public List<Member> createMemberList() {
+			
+			
+		//Member File-------------------------------------------------------------------------------------------------
+		public List<Member> createMemberList() {
 			
 			String memberFile = "data/Members.dat";
 			Scanner m = null;
@@ -136,10 +143,12 @@ import entitites.Product;
 				Member mem = new Member(memberCode, memberType, personCode, memberName, a);
 						
 				//Add member	
-				//memberList.add(mem);
+				ArrayList<Member> memberList = new ArrayList<Member>();
+				memberList.add(mem);
 			}
-			//return memberList;
-		//}
+			
+			return memberList;
+		}
 			
 			
 			
@@ -149,7 +158,7 @@ import entitites.Product;
 			
 			
 			//Products File-------------------------------------------------------------------------------------------------
-		//public List<Product> createProductList() {
+		public List<Product> createProductList() {
 		
 			String productFile = "data/Products.dat";
 			Scanner pr = null;
@@ -161,83 +170,78 @@ import entitites.Product;
 				}
 					
 			int k = Integer.parseInt(pr.nextLine());
-					
-			//System.out.println("The value of n is: " + n);
-					
+										
 			while(pr.hasNext()) {
 				String line = pr.nextLine();
 				String token[] = line.split(";");
-				String productCode = token[0];
-				String productType = token[1];
+				
+				String productCode = "";
+				String productType = "";
+				String street = "";
+				String city = "";
+				String state = "";
+				String zip = "";
+				String country = "";
+				String membershipGroup = "";
+				double costPerUnit = 0;
+				
 				
 				if(token[1].equals("R")) {
 					String equipment = token[2];
 					double equipmentCost = Double.parseDouble(token[3]);
-					System.out.println("Product Code: " + productCode);
-					System.out.println("Product Type: " + productType);
-					System.out.println("Equipment: " + equipment);
-					System.out.println("Equipment Cost: " + equipmentCost);
-					System.out.println("------------------------------------------------");
+
 				} else if (token[1].equals("P")) {
 					double parkingFee = Double.parseDouble(token[2]);
-					System.out.println("Product Code: " + productCode);
-					System.out.println("Product Type: " + productType);
-					System.out.println("Parking Fee: " + parkingFee);
-					System.out.println("------------------------------------------------");
+
 				} else if (token[1].equals("Y")) {
 					String startDate = token[2];
 					String endDate = token[3];
 					String tokenAddress[] = token[4].split(",");
-					String street = tokenAddress[0];
-					String city = tokenAddress[1];
-					String state = tokenAddress[2];
-					String zip = tokenAddress[3];
-					String country = tokenAddress[4];
-					String membershipGroup = token[5];
-					double costPerUnit = Double.parseDouble(token[6]);
-					System.out.println("Product Code: " + productCode);
-					System.out.println("Product Type: " + productType);
-					System.out.println("Start Date: " + startDate);
-					System.out.println("End Date: " + endDate);
-					System.out.println("Address: " + street + ", " + city + ", " + state + ", " + zip + ", " + country);
-					System.out.println("Group: " + membershipGroup);
-					System.out.println("Cost of Membership: " + costPerUnit);
-					System.out.println("------------------------------------------------");
-				} else {			
+					street = tokenAddress[0];
+					city = tokenAddress[1];
+					state = tokenAddress[2];
+					zip = tokenAddress[3];
+					country = tokenAddress[4];
+					membershipGroup = token[5];
+					costPerUnit = Double.parseDouble(token[6]);
+				
+				} else if (token[1].equals("D")){			
 				String dateTime = token[2];
 				String tokenAddress[] = token[3].split(",");
-				String street = tokenAddress[0];
-				String city = tokenAddress[1];
-				String state = tokenAddress[2];
-				String zip = tokenAddress[3];
-				String country = tokenAddress[4];
-				String costPerUnit = token[4];
-				
-				
-				
-				System.out.println("Product Code: " + productCode);
-				System.out.println("Product Type: " + productType);
-				System.out.println("Date & Time: " + dateTime);
-				System.out.println("Address : " + street + ", " + city + ", " + state + ", " + zip + ", " + country);
-				System.out.println("Membership Cost: " + costPerUnit);
-				System.out.println("------------------------------------------------");
+				street = tokenAddress[0];
+				city = tokenAddress[1];
+				state = tokenAddress[2];
+				zip = tokenAddress[3];
+				country = tokenAddress[4];
+				costPerUnit = Double.parseDouble(token[4]);
 				}
 						
 						
-				//Address a = new Address (street, city, state, zip, country);
-				//Person p = new Person(personCode, firstName, lastName, a, email??);
+				Address a = new Address (street, city, state, zip, country);
+				Product prod = new Product(productCode);
 						
 				//Add product	
-				//productList.add(prod);
-				}
-			//return productList;
-		//}
-			
-			
-//Bracket below is for main method
+				ArrayList<Product> productList = new ArrayList<Product>();
+				productList.add(prod);
+			}
+			return productList;
+		}
+		
+		
+		
+	//End of Class
 	}
+			
+			
+	
 	    
-	}
+	
+	
+	
+	
+	
+	
+	
     
 
 
