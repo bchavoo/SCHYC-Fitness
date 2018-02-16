41409a4e3d956f33dacf141bd3c93ba7b991b425
chavezbryanr <chavezbryanr@gmail.com>
2018-02-15 19:39:53 -0600

    Member SubClasses

---
+++ src/entitites/General.java
@@ -0,0 +1,10 @@
+package entitites;
+
+public class General extends Member {
+
+	public General(String memberCode, String memberType, Person contact, String name, Address address) {
+		super(memberCode, memberType, contact, name, address);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/entitites/Student.java
@@ -0,0 +1,10 @@
+package entitites;
+
+public class Student extends Member {
+
+	public Student(String memberCode, String memberType, Person contact, String name, Address address) {
+		super(memberCode, memberType, contact, name, address);
+		// TODO Auto-generated constructor stub
+	}
+
+}
