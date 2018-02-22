6b08cb579eae411c7566406f9bb3744b754a911e
Mohammad Majid <momajid24@gmail.com>
2018-02-22 00:39:21 -0600

    Update Service.java

--- src/product/Service.java
+++ src/product/Service.java
@@ -2,6 +2,14 @@ package product;
 
 public abstract class Service extends Product {
 	
+	/**
+	 * The service is  child class of the Product super class. The super helps us see that the 
+	 *Service INHERENTES attributes of the Service class. 
+	 * @param productCode
+	 * @param cost
+	 * @param productType
+	 */
+	
 	private double cost;
 	private String productType;
 	
@@ -27,6 +35,13 @@ public abstract class Service extends Product {
 	
 
 	//Abstract Methods
+	/**
+	 * Here we create abstract methods to successfully use to override the super class
+	 * and calculations
+	 * @param cost
+	 * @param quantity
+	 * @return
+	 */
 	public abstract double getSubTotal(double cost, double quantity);
 	public abstract double getTax(double cost);
 	public abstract double getTotal(double cost, double tax);
