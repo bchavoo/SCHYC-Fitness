95e0dab883b27a74c2a9f3822dd3d805ddb1dc2f
Mohammad Majid <momajid24@gmail.com>
2018-02-22 01:26:26 -0600

    Update YearMemberships.java

--- src/product/YearMemberships.java
+++ src/product/YearMemberships.java
@@ -6,11 +6,28 @@ import entitites.Address;
 
 public class YearMemberships extends Membership {
 
+
+    /**
+	 * The year membership is a child class of membership so it will inherit all the 
+	 * attributes of the memberships class and its super class
+	 */
+    
+    
 	public DateTime endDate;
 	public String membershipName;
 	public double cost;
 
 	//Constructor
+	/**
+	 * Here we create contructor as well as setter and getters
+	 * @param startDate
+	 * @param endDate
+	 * @param address
+	 * @param membershipName
+	 * @param cost
+	 * @param productCode
+	 * @param productType
+	 */
 	public YearMemberships(DateTime startDate, DateTime endDate, Address address, String membershipName, double cost, String productCode, String productType) {
 		super(productCode, startDate, address, productType);
 		this.endDate = endDate;
@@ -42,6 +59,11 @@ public class YearMemberships extends Membership {
 	public void setCost(double cost) {
 		this.cost = cost;
 	}
+	
+	/**
+	 * Here the the super class is overridden and we used the abstraction method 
+	 * to help us with returning the right calculations
+	 */
 
 	@Override
 	public double getDiscount() {
