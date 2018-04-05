4146b5930e1293d877ce06bf026f9043d1f8f8c7
chavezbryanr <chavezbryanr@gmail.com>
2018-04-04 20:06:48 -0500

    Invoice Report Ordered

--- src/driver/InvoiceReport.java
+++ src/driver/InvoiceReport.java
@@ -11,14 +11,20 @@ import linked.TotalComparator;
 import writer.InvoiceWriter;
 
 public class InvoiceReport {
-	
-public static void main(String[] args) throws IOException {
-		
+
+	public static void main(String[] args) throws IOException {
+
 		//Calls a method to get a list of invoices
 		List<Invoice> DBInvoiceList = DBReader.createInvoiceList();		
-		
+
+
+
 		InvoiceList<Invoice> invoiceOrderedList = new InvoiceList<Invoice>(new TotalComparator());
 		
+		for(int i = 0; i < DBInvoiceList.size(); i++) {
+			invoiceOrderedList.add(DBInvoiceList.get(i));
+		}
+
 		//This will generate invoice report, with a given list of invoices and display it on the console
 		InvoiceWriter.createInvoiceReport((List<Invoice>) invoiceOrderedList);
 
