b90a97233018a5434e672947cdbafd5c54dfeb09
chavezbryanr <chavezbryanr@gmail.com>
2018-04-04 19:46:48 -0500

    Comparator fixes

--- src/linked/TotalComparator.java
+++ src/linked/TotalComparator.java
@@ -9,15 +9,14 @@ public class TotalComparator implements Comparator<Invoice> {
 	@Override
 	public int compare(Invoice inv1, Invoice inv2) {
 		// TODO Auto-generated method stub
-		double id1 = inv1.getTotal();
-		double id2 = inv2.getTotal();
 
-		if(id1 == id2) {
+		if(inv1.getTotal() == inv2.getTotal()) {
 			return 0;
-		} else if (id1 > id2) {
+		} else if (inv1.getTotal() > inv2.getTotal()) {
 			return 1;
-		} else
+		} else {
 			return -1;
 		}
+	}
 
 }
